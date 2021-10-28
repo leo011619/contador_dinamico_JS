51072d43e48e9b1e4373b2d5428f1b2ed579b80c
leo011619 <leonard.ferraz@hotmail.com>
2021-10-28 20:19:00 -0300

    Script JS - função de contar usando for

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-alert("Olá");
+function contar() {
+  var inicio = document.getElementById("txtini");
+  var fim = document.getElementById("txtfim");
+  var passo = document.getElementById("txtpas");
+  var res = document.getElementById("res");
+
+  if (
+    inicio.value.length == 0 ||
+    fim.value.length == 0 ||
+    passo.value.length == 0
+  ) {
+    window.alert("[ERRO] Faltam dados!");
+  } else {
+    res.innerHTML = "Contando:";
+    var i = Number(inicio.value);
+    var f = Number(fim.value);
+    var p = Number(passo.value);
+
+    if (i < f) {
+      for (var c = i; c <= f; c += p) {
+        res.innerHTML += ` ${c} \u{1F449}`;
+      }
+    } else {
+      for (var c = i; c >= f; c -= p) {
+        res.innerHTML += ` ${c} \u{1F449}`;
+      }
+    }
+    res.innerHTML += `\u{1F3C1}`;
+  }
+}
